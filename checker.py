1f2667aee267d2f7ccd1ce4632e762ade4c1ef8d
irina-b-dev <irinabradu.a@gmail.com>
2023-04-18 17:52:44 +0300

    updated checker

--- checker.py
+++ checker.py
@@ -1,9 +1,18 @@
-if __name__ == "__main__":
-    main()
-    pass
+import hashlib
+
+
 
 def main():
     print("This is the checker for the CTF")
-    print("Please enter the flag: ")
+    flag = input("Please enter the flag:\n")
+    hashed_flag = hashlib.sha256(flag.encode('utf-8')).hexdigest()
+ 
+    if(hashed_flag == "7a4e85edf3a5336b9634d4957159b52144b25ac830f711d15a66d8a344cd0644"):
+        print("Well done! :D")
+    else:
+        print("Not the correct flag, try again")
 
-    flag = input()
+if __name__ == "__main__":
+    main()
+    pass
+        
